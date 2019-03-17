68c0c6068d70e01b532e0d6978c9536fe5087162
Mahabbat Mammadov <mahabbatmusic@gmail.com>
2019-03-17 15:28:27 +0400

    Update MainController.php

--- MainController.php
+++ MainController.php
@@ -14,14 +14,47 @@ class MainController extends Controller
             'grant_type' => 'password',
             'client_id' => 2,
             'client_secret' => env('PASSPORT_GRANT'),
-            'username' => request('username'),
-            'password' => request('password'),
+            'username' => $req->email,
+            'password' => $req->password,
             'scope' => '*',
         ]);
 
         $tokenRequest = Request::create('/oauth/token', 'POST');
         $response = Route::dispatch($tokenRequest);
 
-        return dd($response);
+
+        $code = $response->getStatusCode();
+
+        if ($code === 200) {
+            return response()->json(json_decode($response->getContent()), 200);
+        } else if ($code === 400) $error = "Invalid request. Please enter a email or password!";
+        else if ($code === 401) $error = "Your credentials are incorrect!";
+        else $error = "Something went wrong";
+
+        if ($error) return response()->json(['error' => $error], 400);
+    }
+
+    public function register(Request $req)
+    {
+        $req->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        return User::create([
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => bcrypt($req->password)
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens->each(function ($token) {
+            $token->delete();
+        });
+
+        return response()->json(['message' => 'Logged out successfully!'], 200);
     }
 }
